7317ac6228ae0824ceabba21dd303a734af1cb77
kirti arora <111427307+kit0-0@users.noreply.github.com>
2023-07-09 04:11:58 +0000

    Update width of buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function Calculator() {
-  const renderButton = (text, name, color) => (
+  const renderButton = (text, name, color, style) => (
     <div className="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
       <button
         type="button"
-        className={`rounded-full h-12 w-20 flex items-center ${color} justify-center shadow-lg hover:border-gray-500 focus:outline-none`}
+        className={`rounded-full h-12 w-20 flex items-center ${color} justify-center shadow-lg hover:border-gray-500 focus:outline-none ${style}`}
         name={name}
       >
         {text}
@@ -23,9 +23,9 @@ function Calculator() {
         </div>
 
         <div className="flex items-stretch bg-[#c8c8c8] h-20">
-          {renderButton('%', '%', 'bg-[#505050]')}
-          {renderButton('(', '(', 'bg-[#505050]')}
-          {renderButton(')', ')', 'bg-[#505050]')}
+          {renderButton('AC', 'AC', 'bg-gray-800')}
+          {renderButton('+/-', '+/-', 'bg-gray-800')}
+          {renderButton('%', '%', 'bg-gray-800')}
           {renderButton('÷', '/', 'bg-orange-500')}
         </div>
 
@@ -51,10 +51,9 @@ function Calculator() {
         </div>
 
         <div className="flex items-stretch bg-[#c8c8c8] h-20 mb-4">
-          {renderButton('AC', null, 'bg-gray-800')}
-          {renderButton('0', '0', 'bg-gray-800')}
+          {renderButton('0', '0', 'bg-gray-800', 'w-52')}
           {renderButton('.', '.', 'bg-gray-800')}
-          {renderButton('=', null, 'bg-orange-500')}
+          {renderButton('=', '=', 'bg-orange-500')}
         </div>
       </div>
     </div>
