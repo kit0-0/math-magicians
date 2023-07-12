cd321aaedb04c681cb4ccaf8a1c8d5e125cd6923
kirti arora <111427307+kit0-0@users.noreply.github.com>
2023-07-12 21:22:02 +0000

    Add button hover effect

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -17,7 +17,7 @@ function Calculator() {
     <div className="flex-1 px-2 py-2 justify-center flex items-center text-white text-2xl font-semibold">
       <button
         type="button"
-        className={`rounded-full h-12 w-20 flex items-center ${color} justify-center shadow-lg hover:border-gray-500 focus:outline-none ${style}`}
+        className={`hover:bg-sky-700 rounded-full h-12 w-20 flex items-center ${color} justify-center shadow-lg hover:border-gray-500 focus:outline-none ${style}`}
         name={name}
         onClick={() => handleClick(name)}
       >
@@ -27,46 +27,49 @@ function Calculator() {
   );
 
   return (
-    <div className="mx-auto overflow-hidden mt-10 shadow-lg mb-2 bg-[#a4a4a4] shadow-lg border rounded-lg lg:w-2/6 md:w-3/6 sm:w-4/6">
-      <div className="">
-        <div className="p-5 text-white text-right text-3xl bg-[#515151]">
-          <span className="text-white-500">
-            {calculatorState.next || calculatorState.total || '0'}
-          </span>
-        </div>
+    <div>
+      <h1 className="justify-center flex items-center mt-3 text-3xl"> calculate here 👇 </h1>
+      <div className="mx-auto overflow-hidden mt-10 shadow-lg mb-2 bg-[#a4a4a4] border rounded-lg lg:w-2/6 md:w-3/6 sm:w-4/6">
+        <div className="">
+          <div className="p-5 text-white text-right text-3xl bg-[#515151]">
+            <span className="text-white-500">
+              {calculatorState.next || calculatorState.total || '0'}
+            </span>
+          </div>
 
-        <div className="flex items-stretch bg-[#c8c8c8] h-20">
-          {renderButton('AC', 'AC', 'bg-gray-800')}
-          {renderButton('+/-', '+/-', 'bg-gray-800')}
-          {renderButton('%', '%', 'bg-gray-800')}
-          {renderButton('÷', '÷', 'bg-orange-500')}
-        </div>
+          <div className="flex items-stretch bg-[#c8c8c8] h-20">
+            {renderButton('AC', 'AC', 'bg-gray-800')}
+            {renderButton('+/-', '+/-', 'bg-gray-800')}
+            {renderButton('%', '%', 'bg-gray-800')}
+            {renderButton('÷', '÷', 'bg-orange-500')}
+          </div>
 
-        <div className="flex items-stretch bg-[#c8c8c8] h-20">
-          {renderButton('7', '7', 'bg-gray-800')}
-          {renderButton('8', '8', 'bg-gray-800')}
-          {renderButton('9', '9', 'bg-gray-800')}
-          {renderButton('×', 'x', 'bg-orange-500')}
-        </div>
+          <div className="flex items-stretch bg-[#c8c8c8] h-20">
+            {renderButton('7', '7', 'bg-gray-800')}
+            {renderButton('8', '8', 'bg-gray-800')}
+            {renderButton('9', '9', 'bg-gray-800')}
+            {renderButton('×', 'x', 'bg-orange-500')}
+          </div>
 
-        <div className="flex items-stretch bg-[#c8c8c8] h-20">
-          {renderButton('4', '4', 'bg-gray-800')}
-          {renderButton('5', '5', 'bg-gray-800')}
-          {renderButton('6', '6', 'bg-gray-800')}
-          {renderButton('-', '-', 'bg-orange-500')}
-        </div>
+          <div className="flex items-stretch bg-[#c8c8c8] h-20">
+            {renderButton('4', '4', 'bg-gray-800')}
+            {renderButton('5', '5', 'bg-gray-800')}
+            {renderButton('6', '6', 'bg-gray-800')}
+            {renderButton('-', '-', 'bg-orange-500')}
+          </div>
 
-        <div className="flex items-stretch bg-[#c8c8c8] h-20">
-          {renderButton('1', '1', 'bg-gray-800')}
-          {renderButton('2', '2', 'bg-gray-800')}
-          {renderButton('3', '3', 'bg-gray-800')}
-          {renderButton('+', '+', 'bg-orange-500')}
-        </div>
+          <div className="flex items-stretch bg-[#c8c8c8] h-20">
+            {renderButton('1', '1', 'bg-gray-800')}
+            {renderButton('2', '2', 'bg-gray-800')}
+            {renderButton('3', '3', 'bg-gray-800')}
+            {renderButton('+', '+', 'bg-orange-500')}
+          </div>
 
-        <div className="flex items-stretch bg-[#c8c8c8] h-20 mb-4">
-          {renderButton('0', '0', 'bg-gray-800', 'w-52')}
-          {renderButton('.', '.', 'bg-gray-800')}
-          {renderButton('=', '=', 'bg-orange-500')}
+          <div className="flex items-stretch border-2 bg-[#c8c8c8] h-20 mb-3 ml-3 mr-3 gap-5">
+            {renderButton('0', '0', 'bg-gray-800', 'w-52')}
+            {renderButton('.', '.', 'bg-gray-800')}
+            {renderButton('=', '=', 'bg-orange-500')}
+          </div>
         </div>
       </div>
     </div>
